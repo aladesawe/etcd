44431acbbc26b5cb77bfc28e407138d18a28f1ed
adeyemi <adeyemi.aladesawe@gmail.com>
2025-02-05 14:18:43 -0800

    Migrate WALVersion interface to fix unexported-return
    Signed-off-by: adeyemi <adeyemi.aladesawe@gmail.com>

--- server/storage/schema/schema.go
+++ server/storage/schema/schema.go
@@ -22,6 +22,7 @@ import (
 
 	"go.etcd.io/etcd/api/v3/version"
 	"go.etcd.io/etcd/server/v3/storage/backend"
+	"go.etcd.io/etcd/server/v3/storage/wal"
 )
 
 // Validate checks provided backend to confirm that schema used is supported.
@@ -47,10 +48,7 @@ func localBinaryVersion() semver.Version {
 	return semver.Version{Major: v.Major, Minor: v.Minor}
 }
 
-type WALVersion interface {
-	// MinimalEtcdVersion returns minimal etcd version able to interpret WAL log.
-	MinimalEtcdVersion() *semver.Version
-}
+type WALVersion = wal.Version
 
 // Migrate updates storage schema to provided target version.
 // Downgrading requires that provided WAL doesn't contain unsupported entries.
@@ -73,10 +71,7 @@ func UnsafeMigrate(lg *zap.Logger, tx backend.UnsafeReadWriter, w WALVersion, ta
 	if target.LessThan(current) {
 		minVersion := w.MinimalEtcdVersion()
 		if minVersion != nil && target.LessThan(*minVersion) {
-			// Occasionally we may see this error during downgrade test due to ClusterVersionSet,
-			// which is harmless. Please read https://github.com/etcd-io/etcd/pull/13405#discussion_r1890378185.
-			return fmt.Errorf("cannot downgrade storage, WAL contains newer entries, as the target version (%s) is lower than the version (%s) detected from WAL logs",
-				target.String(), minVersion.String())
+			return fmt.Errorf("cannot downgrade storage, WAL contains newer entries")
 		}
 	}
 	return plan.unsafeExecute(lg, tx)
